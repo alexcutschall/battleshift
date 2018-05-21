737887678cd401ec250642b453fdc1b87adbd94e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-20 23:40:13 -0600

    Ship placement spec cleaned up

--- spec/requests/user/user_can_place_their_ship_spec.rb
+++ spec/requests/user/user_can_place_their_ship_spec.rb
@@ -1,39 +1,28 @@
 require 'rails_helper'
 
 describe 'as a user' do
+  let(:user) { create(:activated_user) }
+  let(:user2) { create(:activated_user) }
+  let(:game) { create(:game, player_1: user, player_2: user2, player_1_board: Board.new(4), player_2_board: Board.new(4)) }
+
   context 'they place a ship' do
     it 'is correctly placed' do
-      user, user_2 = create_list(:activated_user, 2)
-      player_1_board = Board.new(4)
-      player_2_board = Board.new(4)
-      game = Game.create(id: 64, player_1_id: user.id,
-        player_2_id: user_2.id, player_1_board: player_1_board, player_2_board: player_2_board)
 
-      conn = Faraday.new(:url => 'http://localhost:3000')
+      headers = {
+        'Content-Type' => 'application/json',
+        'X-API-Key' => user.api_key
+      }
+      body = {
+        'ship_size' => 2,
+        'start_space' => 'A1',
+        'end_space' => 'A2'
+      }.to_json
 
-      response = conn.post do |req|
-        req.url "/api/v1/games"
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['X-API-Key'] = ENV['BATTLESHIFT_API_KEY']
-        req.body = { 'opponent_email': ENV['BATTLESHIFT_EMAIL'] }.to_json 
-      end
+      post "/api/v1/games/#{game.id}/ships", headers: headers, params: body
 
-      expect(response).to be_success
-      game_json = JSON.parse(response.body, symbolize_names: true)
-
-      response = conn.post do |req|
-        req.url "/api/v1/games/#{game_json[:id]}/ships"
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['X-API-Key'] = ENV['BATTLESHIFT_API_KEY']
-        req.body = {
-          'email': ENV['BATTLESHIFT_OPPONENT_EMAIL'],
-          'ship_size': 2,
-          'start_space': "A1",
-          'end_space': "A2"
-        }.to_json
-      end
-
-      expect(response).to be_success
+      expect(response).to be_successful
+      game.reload
+      expect(game.player_1_board.board.flatten.first["A1"].contents).to be_a Ship
     end
   end
 end
